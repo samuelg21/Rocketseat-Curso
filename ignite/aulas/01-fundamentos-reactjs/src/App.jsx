0c078e33165949ac89fb3d84f14f2e96422d0fc6
Zaamasus <samuelgomes22442@gmail.com>
2025-07-03 19:01:46 -0300

    Primeiro commit

---
+++ ignite/aulas/01-fundamentos-reactjs/src/App.jsx
@@ -0,0 +1,16 @@
+import {Post} from "./Post";
+
+
+export function App() {
+  
+
+  return (
+    <div>
+      <Post />
+      <Post />
+      <Post />
+
+    </div>
+  )
+}
+
---
+++ ignite/aulas/01-fundamentos-reactjs/src/Post.jsx
@@ -0,0 +1,4 @@
+export function Post() {
+  return <div>Post</div>;
+}
+
